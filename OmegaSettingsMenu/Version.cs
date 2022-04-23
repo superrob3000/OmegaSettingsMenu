c79745db192bd441f4976e813b941ff82e1da413
superrob3000 <robbracero@gmail.com>
2022-04-23 14:05:02 -0400

    Bumped version to 1.10.1.

--- OmegaSettingsMenu/Version.cs
+++ OmegaSettingsMenu/Version.cs
@@ -3,6 +3,6 @@ namespace OmegaSettingsMenu
 {
     class Version
     {
-        public const string version = "Omega Package v1.10";
+        public const string version = "Omega Package v1.10.1";
     }
 }
